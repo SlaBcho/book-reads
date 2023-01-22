4aa44ec2fe78ce6f3667042472cbf25eb2a3afaf
Slavcho Boyukliev <boyuklievslavcho@gmail.com>
2023-01-22 15:05:28 +0200

    add static details and register page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import { Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header/Header';
 import { Login } from './components/Login/Login';
+import { Register } from './components/Register/Register';
+
 import Main from './components/Main/Main';
+import Details from './components/Details/Details';
 
 function App() {
+
   return (
     <div className="wrapper">
       <Header />
       <Routes>
-        <Route path={'/login'} element={<Login />} />
         <Route path={'/'} element={<Main />} />
+        <Route path={'/login'} element={<Login />} />
+        <Route path={'/register'} element={<Register />} />
+        <Route path={'/details'} element={<Details />} />
       </Routes>
     </div>
   );
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,13 +1,6 @@
-import styles from './Login.module.css';
-import { useNavigate } from 'react-router-dom';
+import styles from './Register.module.css';
 
-export const Login = () => {
-    const navigate = useNavigate();
-
-    const onRegister = () => {
-        navigate('/register');
-    };
-
+export const Register = () => {
     return (
         <div className={styles.container}>
             
@@ -22,18 +15,20 @@ export const Login = () => {
                             <input className={styles['form-control']} type="email" id="email" name="email"></input>
                         </div>
                         <div>
-                            <label for="user_login_password" >Моля въведете парола</label>
+                            <label for="password" >Моля въведете парола</label>
                             <input className={styles['form-control']} type="password" id="passwprd" name="password"></input>
                         </div>
+                        <div>
+                            <label for="repeat-password" >Моля повторете паролата</label>
+                            <input className={styles['form-control']} type="password" id="repeat-passwprd" name="repeat-password"></input>
+                        </div>
 
-                        <input className={styles.continue} type="submit" name="user-login" value="Продължи" />
+                        <input className={styles.continue} type="submit" name="user-login" value="Регистрирай се" />
                     </form>
-                    <div className={styles.text}>
-                       <p> Нямате акаунт? Не се тревожете!</p>
-                        <input onClick={onRegister} className={styles.register} type="submit" name="user-register" value="Регистрация" />
-                    </div>
                 </div>
             </div>
         </div>
     );
 };
+
+export default Register;
