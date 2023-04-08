851188488c3184608b8a1b174e6b65701b4e453f
Slavcho Boyukliev <boyuklievslavcho@gmail.com>
2023-04-08 13:10:22 +0300

    common changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 import { AuthContext } from '../../context/AuthContext';
